080d0e77c44344085fb98e4b6747997a6368a030
RenaldKS <109418521+RenaldKS@users.noreply.github.com>
2026-03-15 12:49:35 +0700

    Update futureplan.js

--- src/data/futureplan.js
+++ src/data/futureplan.js
@@ -13,7 +13,17 @@ const futureplan = [
         title: "Android Kotlin/Golang",
         desc:"Need to expand more knowledge about Android",
         techno: "Android, Kotlin, Golang",
-    }
+    },
+    {
+        title: "Rental Houses Portofolio with Next.js",
+        desc:"To learn about next.js and make a real use case of websites.",
+        techno: "Next.Js, Tailwind CSS, VPS",
+    },
+    {
+        title: "Make 3D Clothes for game content",
+        desc:"Want to Learn making clothes or 3D asset.",
+        techno: "Blender, 3D Modelling, Photoshop",
+    },
     
 ]
 
